3972c0d04faea61b80537fd2680c0dcdb2d3028f
Roman Romanov <rromanov.vk@gmail.com>
2025-11-19 09:30:19 +0500

    clean up

--- MVVMs/MVVM+Delegate/ViewController/DelegateViewController.swift
+++ MVVMs/MVVM+Delegate/ViewController/DelegateViewController.swift
@@ -16,9 +16,7 @@ final class DelegateViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+	required init?(coder: NSCoder) { nil }
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- MVVMs/MVVM+State/ViewController/StateViewController.swift
+++ MVVMs/MVVM+State/ViewController/StateViewController.swift
@@ -19,10 +19,8 @@ final class StateViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+	required init?(coder: NSCoder) { nil }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
